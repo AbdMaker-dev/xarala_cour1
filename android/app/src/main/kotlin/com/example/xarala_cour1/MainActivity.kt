9b29f05b678bc4b9d1f95f4eb258623888aa1ceb
AbdMaker-dev <dioufbadaraalioune7@gmail.com>
2024-01-31 11:03:52 +0000

    projet initial

---
+++ android/app/src/main/kotlin/com/example/xarala_cour1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xarala_cour1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color bleuColor = Color(0xFF0A8ED9);
+const Color lightbleuColor = Color(0xFFA0DAFB);
+const Color witheColor = Color(0xFFFAFAFA);
